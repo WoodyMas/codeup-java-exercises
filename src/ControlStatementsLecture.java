34451ab583f605dcef5fc7586b93b13f74ef9afb
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-12 15:02:47 -0500

    added ControlStatementsLecture file

---
+++ src/ControlStatementsLecture.java
@@ -0,0 +1,9 @@
+public class ControlStatementsLecture {
+    public static void main(String[] args) {
+//        int myInteger = 20;
+        int x = 5;
+        if (x==5){
+            System.out.println("x is 5");
+        }
+    }
+}
