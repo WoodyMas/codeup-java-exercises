06e8eb940b02be8f41fd37315e1a482e4943f581
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-14 10:11:57 -0500

    String Exercise refactored. Need to complete Bob class

---
+++ src/Bob.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        // Any question needs to respond with the answer "sure", and "Whoa, chill out!" if user input ends with an exclamation mark
+        // Empty inputs will be responded with "Fine. Be that way!"
+        // Any other input will be responded with "Whatever."
+
+                // need a scanner so we can accept user input
+                String customerChoice;
+                Scanner scanner = new Scanner(System.in);
+                customerChoice = scanner.nextLine();
+
+            }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class StringExercise {
+    public static void main(String[] args) {
+        String string1 = "We don't need no education\n" +
+                "We don't need no thought control";
+        String string2 = "Check \"this\" out!, \"s inside of \"s!";
+        String string3 = "In windows, the main drive is usually C:\\";
+        String string4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+        String allString = string1 + "\n" + string2 + "\n" + string3 + "\n" + string4;
+
+
+        System.out.printf("%s", allString);
+
+    }
 }
