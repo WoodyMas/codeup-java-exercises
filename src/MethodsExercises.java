ea56845feac1d429c94b6aea671bc21299b59eb2
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-17 11:27:02 -0500

    adding Person java class (Java 2)

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -8,38 +8,59 @@ public class MethodsExercises {
 //        System.out.println("5 * 7 = " + multiplication(5, 7));
 //        System.out.println("40 / 5 = " + division(40, 5));
 //        System.out.println("50 % 5 = " + modulus(50, 5));
-        getInteger(50, 90);
-        getIntegerRecursion(1, 50);
+//        getInteger(50, 90);
+//        getIntegerRecursion(1, 50);
+        int get = getIntegerRecursion(1, 10);
+        int fact = calculateFactorial(get);
+        System.out.println(fact);
+//        System.out.println(twoDice());
+
 
     } // End main
 
         // Addition
         public static int addition (int input, int input2) {
             return input + input2;
-        }
+        } // End Addition
 
         // Subtraction
         public static int subtraction (int input, int input2) {
             return input - input2;
-        }
+        } // End Subtraction
+
         // Multiplication
         public static int multiplication (int input, int input2) {
             return input * input2;
-        }
+        } // End Multiplication
+
         // Division
-        public static int division (int input, int input2) {
+        public static double division (double input, double input2) {
             return input / input2;
-        }
+        } // End Division
+
         // Modulus
         public static int modulus (int input, int input2){
             return input % input2;
+        } // End Modulus
+
+        public static int calculateFactorial (int userInput){
+//            Scanner scanner = new Scanner(System.in);
+//            userInput = Integer.parseInt(scanner.nextLine());
+//
+//            System.out.printf("You chose %d%n", userInput);
+            int count = 1;
+            for (int i = 1; i < userInput+1; i++){
+                count *= i;
+            }
+            return count;
         }
 
-        //Create a method that validates that user input is in a certain range and returns that input as an integer if it is within the given range. If not, prompt the user to input their number again until the input is within range.
-        //The method signature should look like this:
-        //public static int getInteger(int min, int max);
 
         public static int getInteger(int min, int max) {
+        // Create a method that validates that user input is in a certain range and returns that input as an integer
+        // if it is within the given range. If not, prompt the user to input their number again until the input is within range.
+        // The method signature should look like this:
+        // public static int getInteger(int min, int max);
             // declare userInput as an integer. This will make comparing it to min and max possible
             int userInput = 0; // we'll start with 0, though it will be re-assigned during the loop
 
@@ -47,25 +68,27 @@ public class MethodsExercises {
             // We will re-assign its value when userInput is between min and max)
             String continueLoop = "";
 
-            // While continueLoop is an empty string,
-            while (continueLoop.equals("")){
-                // We will scan
-                Scanner scanner = new Scanner(System.in);
-                System.out.printf("Enter an integer within the range of %d - %d%n", min, max);
-                    // We are scanning for userInput
-                userInput = Integer.parseInt(scanner.next());
+                // While continueLoop is an empty string,
+                while (continueLoop.equals("")){
+                    // We will scan
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.printf("Enter an integer within the range of %d - %d%n", min, max);
+                        // We are scanning for userInput
+                    userInput = Integer.parseInt(scanner.next());
 
-                // Check to see if userInput is between min and max
-                if (userInput >= min && userInput <= max) {
-                    System.out.printf("Your input: %d is between the range of %d - %d%n", userInput, min, max);
-                    continueLoop = "n";
-                } else {
-                    // If userInput isn't in the range, we'll display a try again message and the loop will continue
-                    // because the continueLoop string has not been re-assigned
-                    System.out.printf("Your input: %d is NOT between the range of %d - %d%n", userInput, min, max);
-                }
-            }
+                    // Check to see if userInput is between min and max
+                    if (userInput >= min && userInput <= max) {
+                        System.out.printf("Your input: %d is between the range of %d - %d%n", userInput, min, max);
+                        continueLoop = "n";
+                    } else {
+                        // If userInput isn't in the range, we'll display a try again message and the loop will continue
+                        // because the continueLoop string has not been re-assigned
+                        System.out.printf("Your input: %d is NOT between the range of %d - %d%n", userInput, min, max);
+                    }
+                } // End While loop
+
             return userInput;
+
         } // End getInteger old method
 
         // getInteger Recursion method
@@ -81,6 +104,18 @@ public class MethodsExercises {
                 System.out.printf("Your number %d is NOT accepted, as it is NOT within the range of %d-%d%n", userInput, min, max);
                 return getIntegerRecursion(min, max);
             }
+        } // End getIntegerRecursion
+
+        //Create an application that simulates dice rolling.
+
+    //Ask the user to enter the number of sides for a pair of dice.
+        public static double twoDice (){
+
+        return 0;
         }
+    //Prompt the user to roll the dice.
+    //"Roll" two n-sided dice, display the results of each, and then ask the user if he/she wants to roll the dice again.
+    //Use static methods to implement the method(s) that generate the random numbers.
+    //Use the .random method of the java.lang.Math class to generate random numbers.
 
 } // Emd MethodsExercises Class
