d0574ba7dfbbba8130e36aa69272fecf7ed4f8a6
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-20 17:23:59 -0500

    Inheritance and Polymorphism exercise complete

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,5 +1,7 @@
 package shapes;
 
+import javax.swing.*;
+
 public class ShapesTest {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(5, 3);
@@ -13,5 +15,16 @@ public class ShapesTest {
         System.out.println(square.getArea());
         System.out.println(square.getPerimeter());
 
+        System.out.println("Perimeter and Area of Box1");
+        Rectangle box1 = new Rectangle(4, 5);
+        System.out.println(box1.getPerimeter());
+        System.out.println(box1.getArea());
+
+        System.out.println("Perimeter and Area of Box2");
+        Rectangle box2 = new Square(5);
+        System.out.println(box2.getPerimeter());
+        System.out.println(box2.getArea());
+
+        box2.getPerimeter();
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,7 @@
 package shapes;
 
 public class Square extends Rectangle{
-//    private int side;
+    private int side;
 
 //    Create a class named Square, also inside of shapes, that extends Rectangle. Square should define a constructor that accepts one argument, side, and calls the parent's constructor to set both the length and width to the value of side.
 
@@ -18,6 +18,19 @@ public class Square extends Rectangle{
         super.setLength(side);
         super.setWidth(side);
     }
+
+    public int getArea(){
+        int area = (int) Math.pow(side, 2);
+        System.out.printf("New area method for Square: %d%n", area);
+        return area;
+    }
+    public int getPerimeter(){
+        int perimeter = (int) side * 4;
+        System.out.printf("New perimeter method for Square: %d%n", perimeter);
+        return perimeter;
+    }
+
+
 
 
 }
