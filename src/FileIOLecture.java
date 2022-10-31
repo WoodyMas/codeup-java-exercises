a2f9b7d3779d638fbeda355d1b382ef858668143
Mason Wudtee <mason.wudtee@gmail.com>
2022-10-31 11:42:38 -0500

    FileIoOOP added

--- src/FileIOLecture.java
+++ src/FileIOLecture.java
@@ -1,14 +1,17 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileIOLecture {
     public static void main(String[] args) {
 
         // ============================ PATH
-        String path = "src/test.txt.txt"; // Setting a filepath using String data type
-        Path pathToTest = Paths.get("src", "fileIOLecture", "test.txt.txt");
-        System.out.println(pathToTest);
+//        String path = "src/test.txt.txt"; // Setting a filepath using String data type
+//        Path pathToTest = Paths.get("src", "fileIOLecture", "test.txt.txt");
+//        System.out.println(pathToTest);
 
 //        Path pathToWeywot = Paths.get("src", "fileIoLecture", "test.txt.txt");
 //        System.out.println(pathToTest);
@@ -16,9 +19,9 @@ public class FileIOLecture {
 //        Path pathToWeywot = Paths.get("hello", "weywot.txt");
 //        System.out.println(pathToWeywot);
 
-        Path fileLecturePath = Paths.get("src", "fileIoLecture", "FileIOLecture.java");
-        System.out.println(fileLecturePath);
-        System.out.println(fileLecturePath.toAbsolutePath());
+//        Path fileLecturePath = Paths.get("src", "fileIoLecture", "FileIOLecture.java");
+//        System.out.println(fileLecturePath);
+//        System.out.println(fileLecturePath.toAbsolutePath());
 
 //        Path otherPath = Paths.get("src", "../", "src", "../", "src", "fileIoLecture", "FileIOLecture");
 //        System.out.println(otherPath);
@@ -26,10 +29,10 @@ public class FileIOLecture {
 //        System.out.println(otherPath.toAbsolutePath());
 //        System.out.println(otherPath.normalize().toAbsolutePath());
 
-        Path pathToText = Paths.get("src", "practiceTest", "test.txt");
-        System.out.println(pathToText);
-        boolean textExist = Files.exists(pathToText);
-        System.out.println(textExist);
+//        Path pathToText = Paths.get("src", "practiceTest", "test.txt");
+//        System.out.println(pathToText);
+//        boolean textExist = Files.exists(pathToText);
+//        System.out.println(textExist);
 
         // ============================ FILE METHODS
 
@@ -37,21 +40,21 @@ public class FileIOLecture {
 //        System.out.println(Files.exists(fileLecturePath2));
 //        System.out.println(Files.exists(Paths.get("bob.txt")));
 ////
-//        String directory = "data";
-//        String filename = "info.txt";
-//        Path dataDirectory = Paths.get(directory);
-//        Path dataFile = Paths.get(directory, filename);
-////
-//        System.out.println(dataDirectory);
-//        System.out.println(dataFile);
+        String directory = "data";
+        String filename = "info.txt";
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
 //
-//        if (Files.notExists(dataDirectory)) {
-//            try {
-//                Files.createDirectories(dataDirectory);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        System.out.println(dataDirectory);
+        System.out.println(dataFile);
+
+        if (Files.notExists(dataDirectory)) {
+            try {
+                Files.createDirectories(dataDirectory);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
 
 
@@ -67,8 +70,14 @@ public class FileIOLecture {
 
         // ============================ READING FILES
 
+        // p is a Paths.get
 //        Path p = Paths.get("src", "weywot", "java", "test.txt.txt");
-//
+        Path peopleTxtPath = Paths.get("src", "fileIO", "people.txt");
+        System.out.println(peopleTxtPath);
+        System.out.println(Files.exists(peopleTxtPath));
+
+//        System.out.println(p);
+
 //        List<String> lines = new ArrayList<>();
 //
 //        try {
@@ -82,6 +91,7 @@ public class FileIOLecture {
 //        for (String name : lines) {
 //            System.out.println("Hello, " + name + "!");
 //        }
+
 
 
 
